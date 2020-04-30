087d9de30474dad6cb2758680bdc1b05fa9f1457
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-04-30 17:04:56 +0700

    Add handler option feature for CYOA solution

--- cyoa/cyoa.go
+++ cyoa/cyoa.go
@@ -9,50 +9,122 @@ import (
 	"strings"
 )
 
+var tpl *template.Template
+
 func init() {
 	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
 }
 
-var tpl *template.Template
-
 var defaultHandlerTmpl = `
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Choose Your Own Adventure</title>
-</head>
-<body>
-    <h1>{{.Title}}</h1>
-    {{range .Story}}
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Story}}
         <p>{{.}}</p>
-    {{end}}
-    <ul>
-    {{range .Options}}
-        <li><a href="/{{.Arc}}">{{.Text}}</a></li>
-    {{end}}
-    </ul>
-</body>
-</html>
-`
+      {{end}}
+      {{if .Options}}
+        <ul>
+        {{range .Options}}
+          <li><a href="/{{.Arc}}">{{.Text}}</a></li>
+        {{end}}
+        </ul>
+      {{else}}
+        <h3>The End</h3>
+      {{end}}
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FFFCF6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </body>
+</html>`
 
-// NewHandler will return a new http.Handler given a Story
-func NewHandler(s Story) http.Handler {
-	return handler{s}
+// HandlerOption are used with the NewHandler function to configure the http.Handler returned.
+type HandlerOption func(h *handler)
+
+// WithTemplate is an option to provide a custom template for a handler.
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+// WithPathFunc is an option to provide a custom function for processing the story chapter from the incoming request.
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+// NewHandler will return a new http.Handler given a Story and HandlerOption.
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+
+	return h
 }
 
 type handler struct {
-	s Story
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-// ServeHTTP ...
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
-	path = path[1:]
+
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 
 	if chapter, ok := h.s[path]; ok {
 		err := tpl.Execute(w, chapter)
@@ -66,7 +138,7 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Chapter not found", http.StatusNotFound)
 }
 
-// JSONStory will return a Story given a io.Reader
+// JSONStory will return a Story given a io.Reader.
 func JSONStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
 	var story Story
@@ -77,7 +149,7 @@ func JSONStory(r io.Reader) (Story, error) {
 	return story, nil
 }
 
-// Story represents a story
+// Story represents a story.
 type Story map[string]Chapter
 
 // Chapter represents a part of a story.
@@ -87,7 +159,7 @@ type Chapter struct {
 	Options []Option `json:"options"`
 }
 
-// Option of a section
+// Option of a section.
 type Option struct {
 	Text string `json:"text"`
 	Arc  string `json:"arc"`
